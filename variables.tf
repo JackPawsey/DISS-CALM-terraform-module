c16185ea95f3ced9eb7cfc3bb2665599affbd105
JackPawsey <jfrewer@live.co.uk>
2021-03-09 12:49:18 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,16 +14,32 @@ variable "logstash_instance_type" {
   description = "Type of Logstash EC2 instance."
 }
 
+variable "logstash_storage_size" {
+  description = "Size of Logstash EC2 instance disk."
+}
+
 variable "prometheus_instance_type" {
   description = "Type of Prometheus EC2 instance."
+}
+
+variable "prometheus_storage_size" {
+  description = "Size of Prometheus EC2 instance disk."
 }
 
 variable "alertmanager_instance_type" {
   description = "Type of Alertmanager EC2 instance."
 }
 
+variable "alertmanager_storage_size" {
+  description = "Size of Alertmanager EC2 instance disk."
+}
+
 variable "grafana_instance_type" {
   description = "Type of Grafana EC2 instance."
+}
+
+variable "grafana__storage_size" {
+  description = "Size of Grafana_ EC2 instance disk."
 }
 
 variable "elasticsearch_configuration" {
